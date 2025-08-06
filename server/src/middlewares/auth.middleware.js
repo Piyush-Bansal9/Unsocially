9b773fb97a4cb3a880ac1392ff59be608418d6a0
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2025-08-06 19:17:59 +0530

    Db fix

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -4,46 +4,37 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
 
-// Protected Routes
-export const verifyJWT = asyncHandler( async (req, _, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
         const token =
-            req.cookies?.accessToken || req.SideBar("Authorization")?.replace("Bearer ", "");
+            req.cookies?.accessToken || req.get("Authorization")?.replace("Bearer ", "");
         if (!token) {
             throw new ApiError(401, "Unauthorized, Please Login First");
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-        // ! Adding User To Request Object - For Protected Routes
-        const user = await User.findById(decodedToken.id).select("-password")
+        const user = await User.findById(decodedToken.id).select("-password");
         if (!user) {
             throw new ApiError(401, "Invalid access token");
         }
 
         req.user = user;
         next();
-    }
-    catch (error) {
+    } catch (error) {
         throw new ApiError(401, error?.message || "Unauthorized, Invalid Access Token");
     }
 });
 
-
-// ! Will Explore Its Use-Case Later
-// Middleware to check if user is logged in, for unprotected routes, if no user  then fail silently
-export const getLoggedInUserOrIgnore = asyncHandler( async (req, _, next) => {
-    const token =  req.cookies?.accessToken 
-                || req.SideBar("Authorization")?.replace("Bearer ", "");
+export const getLoggedInUserOrIgnore = asyncHandler(async (req, _, next) => {
+    const token =
+        req.cookies?.accessToken || req.get("Authorization")?.replace("Bearer ", "");
 
     try {
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        const user = await User.findById(decodedToken.token).select("-password")
+        const user = await User.findById(decodedToken.token).select("-password");
         req.user = user;
         next();
-    } 
-    catch (error) {
-        // Fail silently with req.user being falsy
-        next();
+    } catch (error) {
+        next(); // fail silently
     }
 });
